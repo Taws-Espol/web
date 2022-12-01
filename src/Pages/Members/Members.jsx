1858c76b6991ff42569012611641d7aed20557f3
Geovanny Rojas Lindao <geovanny_rl14@hotmail.com>
2022-11-30 22:38:18 -0500

    Paginas

---
+++ src/Pages/Members/Members.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+const Members = () => {
+  return (
+    <div className="Members">
+      <Navbar />
+      <h1>Bienvenido a Miembros</h1>
+    </div>
+  );
+};
+
+export default Members;
---
+++ src/Pages/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+const Projects = () => {
+  return (
+    <div className="Projects">
+      <Navbar />
+      <h1>Bienvenido a Proyectos</h1>
+    </div>
+  );
+};
+
+export default Projects;
