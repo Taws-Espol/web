91483d9dafa6d0f5454ea27a5efa64ca428cd012
Geovanny Rojas Lindao <61295483+iGioRojas@users.noreply.github.com>
2023-06-18 17:51:27 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -38,6 +38,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} basename={process.env.PUBLIC_URL}/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
