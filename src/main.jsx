85b00c27ee560bb95b10acb9397009c6afb0d688
Jonathan García <jonfrgar@espol.edu.ec>
2023-07-16 12:05:39 -0500

    Fix deploy url
    Solo sirve para que se pueda recargar el inicio

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Navigate
-} from "react-router-dom";
-import App from './App'
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import App from './App';
 import Members from './Pages/Members/Members';
 import Projects from './Pages/Projects/Projects';
 import About from './Pages/About/About';
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/miembros",
-    element: <Members/>,
-  },
-  // {
-  //   path: "/proyectos",
-  //   element: <Projects/>,
-  // },
-  {
-    path: "/about",
-    element: <About/>,
-  },
-  {
-    path: "/*",
-    element: <Navigate to="/"/>,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+    <HashRouter basename="/frontend-taws">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/miembros" element={<Members />} />
+        <Route path="/proyectos" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
