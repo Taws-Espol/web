28c79e8fd44608624a01d803c3333b17a876ace3
KevinFernandez21 <136862908+KevinFernandez21@users.noreply.github.com>
2023-11-10 21:12:09 -0500

    Centrar Iconos de seccion de Proyectos

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,11 +6,15 @@ import {BsFileCode} from 'react-icons/bs'
 const Projects = () => {
     return(
         <div className= 'Proyects'>
-            <h1 className='text-tawsBlue font-semibold text-4xl mt-24 mb-4'><span className='text-white'>$~</span> Proyectos</h1>
+            <h1 className='text-tawsBlue font-semibold text-4xl mt-24 mb-4'>
+                <span className='text-white'>$~</span> Proyectos
+            </h1>
             <div className="grid grid-cols-2 max-sm:grid-cols-1">
                 { projects.map((project) => {
                     return <div className="mx-12 flex-col py-8 text-white">
-                    <span class="icon2 fas fa-globe bg-tawsLight">{<BsFileCode size={30} class="flex items-center"/>}</span>
+                    <span class="icon2 fas fa-globe bg-tawsLight flex items-center justify-center">
+                        {<BsFileCode size={30} class="flex items-center"/>}
+                    </span>
                     <h2 className='text-xl font-bold mb-3 mt-16' >{project.nombre}</h2>
                     <p className='my-3' >{project.descripcion}</p>
                     <p className='mt-4 bg-blue-600'>Miembros: {project.miembros}</p>
