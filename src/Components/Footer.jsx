40c7e2790520cc8581dd4dafc46649c33db2c782
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2023-11-17 22:06:34 -0500

    [FIX] el footer se modificó para que la sección de contactos e iconos sean mas amigables en mobile

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
         <div className='Footer mt-24'>
             <div className="grid grid-cols-2 max-sm:grid-cols-1 max-sm:gap-10 pb-6" >
                 <div className="contacto">
-                    <h1 className='text-white font-bold text-4xl pb-4'>Contáctanos</h1>
+                    <h1 className='text-white text-center font-bold text-4xl pb-4 md:text-left'>Contáctanos</h1>
                     <h2 className='text-white'>Bloque 15 - FIEC, Guayaquil 090150, Ecuador<br></br>taws@espol.edu.ec</h2>
                 </div>
                 <div className="redes">
-                    <h1 className='text-white font-bold text-4xl pb-4'>Redes Sociales</h1>
-                    <ul className="flex">
+                    <h1 className='text-white text-center font-bold text-4xl pb-4 md:text-left'>Redes Sociales</h1>
+                    <ul className="flex justify-around lg:justify-normal">
                         <li className='mr-4'><SocialIcon url="https://twitter.com/TawsEspol" fgColor="#fff"/></li>
                         <li className='mr-4'><SocialIcon url="https://www.facebook.com/tawsespol" fgColor="#fff"/></li>
                         <li className='mr-4'><SocialIcon url="https://github.com/Taws-Espol" fgColor="#fff"/></li>
