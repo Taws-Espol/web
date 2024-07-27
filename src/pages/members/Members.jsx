4f8b88d0ab15cd076fd9998d1c463cb5948ff4c7
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-07-27 09:50:10 -0500

    format

--- src/pages/members/Members.jsx
+++ src/pages/members/Members.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardHeader, CardBody, Typography } from "@material-tailwind/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+} from "@material-tailwind/react";
 import "./members.css";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
@@ -15,7 +20,7 @@ function Header() {
 
 function Cards() {
   const ITEMS_PER_PAGE = 6;
-  const [textoBusqueda, setTextoBusqueda] = useState('');
+  const [textoBusqueda, setTextoBusqueda] = useState("");
   const [active, setActive] = useState(1);
   const [totalPages, setTotalPages] = useState(5);
   const [currentPageItems, setCurrentPageItems] = useState([]);
@@ -45,14 +50,16 @@ function Cards() {
   };
 
   useEffect(() => {
-    if (textoBusqueda === '') {
+    if (textoBusqueda === "") {
       setCurrentPageItems(getCurrentPageItems());
       setTotalPages(Math.ceil(members.length / ITEMS_PER_PAGE));
 
       return;
     }
 
-    const filteredItems = members.filter((item) => item.toLowerCase().includes(textoBusqueda.toLowerCase()));
+    const filteredItems = members.filter((item) =>
+      item.toLowerCase().includes(textoBusqueda.toLowerCase()),
+    );
 
     setTotalPages(Math.ceil(filteredItems.length / ITEMS_PER_PAGE));
     setCurrentPageItems(filteredItems.slice(0, ITEMS_PER_PAGE));
@@ -82,7 +89,10 @@ function Cards() {
       </div>
       <div className="flex flex-wrap justify-center gap-6">
         {currentPageItems.map((item, index) => (
-          <Card className="w-64 bg-white rounded-lg overflow-hidden shadow-lg flex flex-col justify-between" key={index}>
+          <Card
+            className="w-64 bg-white rounded-lg overflow-hidden shadow-lg flex flex-col justify-between"
+            key={index}
+          >
             <CardHeader floated={false} className="h-48">
               {/* TODO: MAKE THIS DYNAMIC */}
               <img
@@ -93,14 +103,20 @@ function Cards() {
             </CardHeader>
             <CardBody className="text-center flex flex-col justify-between h-full">
               <div>
-                <Typography variant="h5" color="blue-gray" className="pt-4 mb-2">
+                <Typography
+                  variant="h5"
+                  color="blue-gray"
+                  className="pt-4 mb-2"
+                >
                   {item}
                 </Typography>
                 <Typography color="blue-gray" className="font-medium">
                   Ing. en computación
                 </Typography>
                 <Typography color="gray" className="text-justify mt-2 p-3">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                  exercitationem praesentium nihil.
                 </Typography>
               </div>
               <div className="mt-2">
@@ -114,7 +130,7 @@ function Cards() {
       </div>
       <div className="flex items-center justify-center py-4">
         <button
-          className={`px-3 py-1 rounded-md text-white mr-2 ${active === 1 ? 'bg-gray-300' : 'bg-tawsBlue'}`}
+          className={`px-3 py-1 rounded-md text-white mr-2 ${active === 1 ? "bg-gray-300" : "bg-tawsBlue"}`}
           onClick={prev}
           disabled={active === 1}
         >
@@ -124,7 +140,7 @@ function Cards() {
           Page {active} of {totalPages}
         </p>
         <button
-          className={`px-3 py-1 rounded-md text-white ml-2 ${active === totalPages ? 'bg-gray-300' : 'bg-tawsBlue'}`}
+          className={`px-3 py-1 rounded-md text-white ml-2 ${active === totalPages ? "bg-gray-300" : "bg-tawsBlue"}`}
           onClick={next}
           disabled={active === totalPages}
         >
