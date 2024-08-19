d1b908b2cbe88b6031227bf059ad0b1e7e6c1d5f
Jorge Apolo <japolo1503@gmail.com>
2024-08-19 05:18:59 -0500

    Added images and descriptions, still some missing. Added images.js

---
+++ src/data/images.js
@@ -0,0 +1,14 @@
+const images = {
+    mentor: require("src/assets/Carmen_Vaca.png"), //Carmen Vaca
+    president: require("src/assets/Victor_Borbor.png"), //Victor Borbor
+    academicvp: require("src/assets/Angelo_Zurita.jpg"), //Angelo Zurita
+    administrativevp: require("src/assets/Samira_Carrillo.png"), //Samira Carrillo
+    hr: require("src/assets/Diego_Salazar.png"), //Diego Salazar
+    welfare: require("src/assets/Nahin_Cevallos.png"), //Nahin Cevallos
+    extrelations: require("src/assets/Francisco_Teran.jpg"), //Francisco Terán
+    academia: require("src/assets/Alex_Otero.jpg"), //Alex Otero
+    arts: require("src/assets/Maria_Jose_Moyano.jpg"), //María José Moyano
+    webpage: require("src/assets/Braulio_Rivas.jpg"), //Braulio Rivas
+  };
+  
+  export default images;
