d9bd7dbb9100c0d7c8c43086288a893620cdd5fd
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-09-11 11:54:41 -0500

    more space for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,12 +8,12 @@ const Projects = () => {
       <h1 className="text-tawsBlue font-semibold text-4xl mt-24 mb-4">
         <span className="text-white">$~</span> Proyectos
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
         {projects.map((project, key) => {
           return (
             <div
               key={key}
-              className="mx-12 flex-col py-8 px-12 rounded-xl text-white bg-projectBG text-wrap"
+              className="m-4 flex-col py-8 px-4 xl:px-12 rounded-xl text-white bg-projectBG text-wrap"
             >
               <span className="icon2 fas fa-globe bg-tawsLight flex items-center justify-center">
                 <BsFileCode size={30} classname="flex items-center" />
