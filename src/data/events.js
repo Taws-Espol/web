151c4f09cf77de65908b86829ef224ab27ae3138
Darvier88 <dpacheco@fiec.espol.edu.ec>
2024-09-14 22:53:38 -0500

    Actu de imagenes, año y actividades

---
+++ src/data/events.js
@@ -0,0 +1,42 @@
+import charlaSalesforce from "../assets/activities/charlaSalesforce.jpg";
+import disenoGrafico from "../assets/activities/diseñoGrafico.jpg";
+import spaceApps from "../assets/activities/spaceApps.jpg";
+import webScrapping from "../assets/activities/webScrapping.jpg";
+import animarExtranjero from "../assets/activities/animarExtranjero.jpg";
+
+export const events = [
+  {
+    name: "Charla - Salesforce",
+    date: "2023-10-10",
+    description: "Aprende sobre como Salesforce puede ser un herramienta útil",
+    img: charlaSalesforce,
+  },
+  {
+    name: "Taller - Diseño Grafico",
+    date: "2024-08-04",
+    description:
+      "En este taller, te adentrarás en la importancia del diseño gráfico y cómo implementarlo",
+    img: disenoGrafico,
+  },
+  {
+    name: "Charla - NASA Space Apps Challenge",
+    date: "2023-10-25",
+    description:
+      "Aprende de qué trata el NASA Space Apps Challenge, y recibe consejos importantes para la participación en este",
+    img: spaceApps,
+  },
+  {
+    name: "Taller - Introducción al Web scrapping y visualización de datos con Plotly",
+    date: "2023-08-18",
+    description:
+      "En este taller, te sumergirás en el mundo del web scraping básico, una habilidad de gran utilidad para la captura de datos y la creación de conjuntos de datos. A través de Python y la potente librería Pandas, adquirirás destrezas para procesar y gestionar datos. Aprenderás a realizar una limpieza fundamental de tus datos, seguida de un análisis y visualización enriquecedora mediante diversos tipos de gráficos. Así, podrás dar sentido y profundidad a los datos obtenidos, brindando una perspectiva valiosa.",
+    img: webScrapping,
+  },
+  {
+    name: "Charla - Animar a realizar prácticas o buscar empleos en el extranjero",
+    date: "2024-07-13",
+    description:
+      "La empresa Jelou desea hablar sobre la IA y sus aplicaciones en el mercado actual a la vez que desea introducirse a los estudiantes.",
+    img: animarExtranjero,
+  },
+];
