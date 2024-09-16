639c8f884c82abba09bce56556f3e5dc9e2155fb
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-09-16 12:20:16 -0500

    cherry pick #57 commits and remove a few icons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Main = () => {
   return (
     <div className="Main grid grid-cols-1 justify-items-center max-w-full">
-      <div className="flex-col w-4/5 mt-20">
+      <div className="flex-col w-4/5 mt-8">
         <div className="flex flex-row flex-wrap">
           <div className="lg:basis-1/2 max-w-full">
             <h1 className="text-white font-medium text-5xl sm:text-6xl">
