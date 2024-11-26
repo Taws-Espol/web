f25e0ba2eb2b3465c83cd450c86b9952a3d61ff5
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-11-25 22:17:41 -0500

    add more events

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -29,37 +29,17 @@ export const EventCard = ({ event, index }) => {
       </div>
       <div className="p-4 flex flex-col my-4">
         <h2 className="text-lg font-semibold">{event.name}</h2>
-        <span className="flex items-center gap-2 font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-4 w-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-            />
-          </svg>
-          <p className="text-sm">Auditorio FIEC</p>
-        </span>
+        <span className="flex items-center gap-2 font-medium"></span>
         <p className="mt-4 text-sm whitespace-pre-wrap">{event.description}</p>
       </div>
-      <div className="flex flex-col justify-center items-center text-center mb-4">
-        <p className="text-xl font-extrabold">{year}</p>{" "}
-        {/* Mostrar el año encima del mes */}
-        <p className="text-2xl font-extrabold">{mes}</p>
-        <p className="text-4xl text-cargo font-medium">{dia}</p>
-        <p className="text-2xl font-medium">10:00</p>
-      </div>
+      {year && mes && dia && (
+        <div className="flex flex-col justify-center items-center text-center mb-4">
+          <p className="text-xl font-extrabold">{year}</p>{" "}
+          {/* Mostrar el año encima del mes */}
+          <p className="text-2xl font-extrabold">{mes}</p>
+          <p className="text-4xl text-cargo font-medium">{dia}</p>
+        </div>
+      )}
     </div>
   );
 };
--- src/data/events.js
+++ src/data/events.js
@@ -3,8 +3,65 @@ import disenoGrafico from "../assets/activities/diseñoGrafico.jpg";
 import spaceApps from "../assets/activities/spaceApps.jpg";
 import webScrapping from "../assets/activities/webScrapping.jpg";
 import animarExtranjero from "../assets/activities/animarExtranjero.jpg";
+import feriaDeCarreras from "../assets/activities/feria_de_carreras_2024.webp";
+import induccion from "../assets/activities/induccion_a_novatos_2024_2s.webp";
+import fundacion from "../assets/activities/sanchez_aguilar.jpg";
+import innovat from "../assets/activities/innovat_latam.webp";
+import ieeextreme from "../assets/activities/ieee_xtreme_2024.webp";
+import nosystemissafe from "../assets/activities/nosystem.webp";
+import ctf from "../assets/activities/ctf.webp";
 
 export const events = [
+  {
+    ctf: "Capture The Flag",
+    date: "2024-11-30",
+    description:
+      'No System Is Safe presenta CTF (Capture The Flag) donde decenas de personas pondrán a prueba sus habilidades en ciberseguridad aprendidas a lo largo de los talleres dados en No System is Safe. ¿Qué es un CTF? Es un desafío en el que los participantes deben resolver problemas y retos de seguridad para obtener "flags" y demostrar su dominio en la ciberseguridad.',
+    img: ctf,
+  },
+  {
+    name: "No System is Safe",
+    date: "2024-11-05",
+    description:
+      "El mayor evento de Ciberseguridad de Noviembre organizado por Taws junto a Célula Microsoft y Codes. Todo tipo de charlas de expertos en seguridad informática, talleres prácticos para dominar herramientas y técnicas de hacking. Además, de preparación para el CTF (Capture The Flag).",
+    img: nosystemissafe,
+  },
+  {
+    name: "Innova-T Latam",
+    date: "2024-11-05",
+    description:
+      "Los miembros del Club TAWS tuvieron una activa participación en el Innova LATAM, la plataforma líder en innovación empresarial en América Latina. Nuestros representantes se sumergieron en una experiencia enriquecedora, descubriendo las últimas tendencias y herramientas en innovación y aprendiendo de las mejores prácticas de empresas exitosas.",
+    img: innovat,
+  },
+  {
+    name: "Taws y Fundación Sánchez Aguilar",
+    date: "2024-10-29",
+    description:
+      "Impactando vidas a través de la educación en tecnología! El Club TAWS se unió a la Fundación Sánchez Aguilar para llevar conocimientos de Machine Learning y Ciencia de Datos a niños talentosos. Nuestros voluntarios compartieron su pasión por la tecnología y guiaron a los niños en actividades y proyectos prácticos.",
+    img: fundacion,
+  },
+  {
+    name: "IEEEXtreme 2024",
+    date: "2024-10-26",
+    description:
+      "Nuestros talentosos miembros participaron en el desafío global de programación #IEEEXtreme, organizado por la IEEE. Durante 24 horas intensas, compitieron contra otros estudiantes para resolver problemas de programación desafiantes.",
+    img: ieeextreme,
+  },
+
+  {
+    name: "Inducción a Novatos 2024-2S",
+    date: "2024-09-25",
+    description:
+      "En la inducción de novatos 2024-2S, nos sumergimos en el fascinante mundo de la 'Ciudad Perdida'. Fue una oportunidad única para presentarnos como Club TAWS y compartir las ventajas y oportunidades que les esperan si se unen a nosotros. Un agradecimiento especial a nuestros dedicados miembros del club que se unieron a esta actividad, ¡contribuyendo a despertar el interés de los nuevos estudiantes por las emocionantes actividades que ofrecemos!",
+    img: induccion,
+  },
+  {
+    name: "Feria de Carreras 2024",
+    date: "2024-09-11",
+    description:
+      "Taws estuvo presente en la Feria de Carreras de 2024 en Espol, donde representó a la carrera de Computación, presentado proyectos y experiencia de los estudiantes.",
+    img: feriaDeCarreras,
+  },
   {
     name: "Taller - Diseño Grafico",
     date: "2024-08-04",
